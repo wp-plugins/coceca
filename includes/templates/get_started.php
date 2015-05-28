50c3872674c096806465736a51459ec9bdf2bad2
CoCeCa <CoCeCa@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-28 07:00:38 +0000

    Removed Update Checker from new version ...
    git-svn-id: https://plugins.svn.wordpress.org/coceca/tags/1.1.1@1169126 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/templates/get_started.php
@@ -0,0 +1,10 @@
+<?php $classes = 'welcome-panel'; ?>
+<div id="welcome-panel" class="<?php echo esc_attr( $classes ); ?>">
+    <div class="welcome-panel-content">
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    </div>
+</div>
+
+<p class="support_text_cta">
+    <a target="_blank" href="http://coceca.com/">Get Started</a>&nbsp; | &nbsp; <a target="_blank" href="http://coceca.com/help/">Support</a>
+</p>
